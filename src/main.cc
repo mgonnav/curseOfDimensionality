2b95f508ddfa8a3372f29dde6deafd487916bb12
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2020-09-04 16:31:31 -0500

    Moved wrote euclidean distance calculation inline.

--- src/main.cc
+++ src/main.cc
@@ -3,14 +3,6 @@
 #include <math.h>
 #include <vector>
 
-int calculateEuclideanDist(std::vector<int> a, std::vector<int> b)
-{
-  auto squareSum = 0LL;
-  for (size_t i = 0; i < a.size(); i++)
-    squareSum += (a[i] - b[i]) ^ 2;
-  return sqrt(squareSum);
-}
-
 int main()
 {
   srand(time(0));
@@ -30,10 +22,16 @@ int main()
         x = rand() % 32767;
     }
 
+    double dist{};
     auto startTime = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < totalData; i++)
-      for (int j = i + 1; j < totalData; j++)
-        calculateEuclideanDist(data[i], data[j]);
+      for (int j = i; j < totalData; j++)
+      {
+        dist = 0;
+        for (size_t k = 0; k < dimensions; k++)
+          dist += (data[i][k] - data[j][k]) ^ 2;
+        dist = sqrt(dist);
+      }
     auto endTime = std::chrono::high_resolution_clock::now();
 
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
